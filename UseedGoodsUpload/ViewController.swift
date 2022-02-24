bff8a58f7f3abf10886cd3a0950dab8f12ee9ab6
joensumin <soomis7@naver.com>
2022-02-24 21:18:18 +0900

    Initial Commit

---
+++ UseedGoodsUpload/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UseedGoodsUpload
+//
+//  Created by Terry on 2022/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
